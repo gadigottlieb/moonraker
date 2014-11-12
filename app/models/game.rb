4febd746036f1a9ee1ddcdf79c6286c85f93835c
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-12 10:55:21 -0500

    made models related

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game < ActiveRecord::Base
   has_many :players
   belongs_to :season
+
+  def record(win_result)
+    wins = 0
+    losses = 0
+    win_result == true ? wins += 1 : losses += 1
+  end
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -1,3 +1,8 @@
 class Season < ActiveRecord::Base
   has_many :games
+  has_and_belongs_to_many :players
+
+  def percentage(wins, losses)
+    (wins.to_f / (losses+wins).to_f).round(3)
+  end
 end
