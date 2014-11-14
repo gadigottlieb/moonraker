5138fa5e8141a510a1722619a5e253a9fb0407dc
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-14 16:18:49 -0500

    Edited the associations between models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,5 @@
 class Game < ActiveRecord::Base
-  has_many :players
   belongs_to :season
-
-  def record(win_result)
-    wins = 0
-    losses = 0
-    win_result == true ? wins += 1 : losses += 1
-  end
+  has_and_belongs_to_many :players
+  has_many :statistics, through: :players  
 end
