7fbb7760a6f2890c95c1b6d60f09315a3cf5e9f1
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-14 16:19:19 -0500

    Edited the association between models

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,8 +1,10 @@
 class Season < ActiveRecord::Base
   has_many :games
+  has_many :statistics, through: :players
   has_and_belongs_to_many :players
 
   def percentage(wins, losses)
-    (wins.to_f / (losses+wins).to_f).round(3)
+    string = (wins.to_f / (losses+wins).to_f).round(3)
+    sprintf('%.3f', string)
   end
 end
