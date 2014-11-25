bf06a9dcde99bbb2f64e1959e772eb41e4691038
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-24 22:15:41 -0500

    enabled stat to belong to season and game

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,5 +1,7 @@
 class Statistic < ActiveRecord::Base
   belongs_to :player
+  belongs_to :season
+  belongs_to :game
 
   def batting_avg(hits, at_bats)
     string = (hits.to_f / at_bats.to_f).round(3)
@@ -20,31 +22,4 @@ class Statistic < ActiveRecord::Base
     string = (hits.to_f + walks.to_f) / (at_bats.to_f + walks.to_f + sac_fly.to_f)
     sprintf('%.3f', string)
   end
-
-  def check_for_doubles(doubles)
-    doubles > 0 ? "2B: #{doubles}" : ""
-  end
-
-  def check_for_triples(triples)
-    doubles > 0 ? "3B: #{triples}" : ""
-  end
-
-  def check_for_homers(homers)
-    doubles > 0 ? "HR: #{homers}" : ""
-  end
-
-  def check_for_rbis(rbis)
-    doubles > 0 ? "RBI: #{rbis}" : ""
-  end
-
-  def check_for_sf(sac_fly)
-    doubles > 0 ? "SF: #{sac_fly}" : ""
-  end
-
-  def season_stats(array)
-    array.each do |stat|
-      stat
-    end
-  end
-
 end
