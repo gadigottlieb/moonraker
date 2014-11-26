40d09d5b5d39af51f5a0a14291738feebd9a248e
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-25 22:34:17 -0500

    enabled statistics to be nested as an attribute under game model.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,50 +3,12 @@ class Game < ActiveRecord::Base
   has_and_belongs_to_many :players
   has_many :statistics, through: :players
 
-  def team_hits(single_game)
-    array = []
-    team_hits = single_game.statistics.map do |stat|
-      if stat.game_id == single_game.id
-        array.push(stat.hits)
-      end
-    end
-      @team_hits = array.inject(:+)
-  end
-
-  def form_date(date)
+  accepts_nested_attributes_for :statistics
+  def format_date(date)
     month = Date::MONTHNAMES[date.month]
     day = date.day
     year = date.year
     month.to_s + " " + day.to_s + ", " + year.to_s
   end
 
-  def check_game_stats(game_array)
-    @doubles = []
-    @triples = []
-    @homers = []
-    # rbis = []
-    # sf = []
-    game_array.statistics.map do |stat|
-      if stat.game_id == game_array.id
-        if stat.homers > 1
-          @homers.push(Player.find(stat.player_id).last_name + ", " + Player.find(stat.player_id).first_name[0] + " " + stat.homers.to_s)
-        elsif stat.homers == 1
-          @homers.push(Player.find(stat.player_id).last_name + ", " + Player.find(stat.player_id).first_name[0])
-          @homers.each do |homer|
-            puts "HR: #{homer}"
-          end
-        elsif stat.doubles > 1
-          @doubles.push(Player.find(stat.player_id).last_name + ", " + Player.find(stat.player_id).first_name[0] + " " + stat.doubles.to_s)
-        elsif stat.doubles == 1
-          @doubles.push(Player.find(stat.player_id).last_name + ", " + Player.find(stat.player_id).first_name[0])
-        elsif stat.triples > 1
-          @triples.push(Player.find(stat.player_id).last_name + ", " + Player.find(stat.player_id).first_name[0] + " " + stat.triples.to_s)
-        elsif stat.triples > 1
-          @triples.push(Player.find(stat.player_id).last_name + ", " + Player.find(stat.player_id).first_name[0])
-        end
-        # @doubles
-        # @triples
-      end
-    end
-  end
 end
