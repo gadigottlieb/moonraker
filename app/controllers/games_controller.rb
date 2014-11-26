723cb2086f9e2228fe5008e5c6992004de11cef5
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-25 22:32:23 -0500

    Updated a games show page and started the form to add a new game to a season

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
 
   def show
+    @season = Season.find(params[:id])
     @game = Game.find(params[:id])
     @players = @game.players
     @players.each do |player|
@@ -9,16 +10,37 @@ class GamesController < ApplicationController
   end
 
   def new
-    @game = Game.new
+    @season = Season.find(params[:season_id])
+    @game = Game.new(season: @season)
   end
 
   def create
+    @season = Season.find(params[:season_id])
     @game = Game.create(game_params)
     if @game.save
-      redirect_to game_path(@game)
+      redirect_to season_game_path(game: @game.id, season: @game.season_id)
     else
       render :new
     end
   end
 
+  def edit
+    @game = Game.find(params[:id])
+    @season = Season.find(@game.season_id)
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    if @game.save
+      redirect_to season_game_path(game: @game.id, season: @game.season_id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def game_params
+    game_params.require(:game).permit(:opponent, :opponent_score, :home_score, statistics_attributes: [:played, :plate_appearance, :runs, :hits, :doubles, :triples, :homers, :rbis, :walks, :hit_by_pitch, :strikeouts, :sac_fly, :player_id ])
+  end
 end
