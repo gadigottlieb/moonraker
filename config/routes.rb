542d28dde8ec3571ad4b71acdea991f2e9083432
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-25 22:32:40 -0500

    Nested games routes under seasons

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
-  resources :games
-  resources :seasons
+  resources :seasons do
+    resources :games
+  end
   post 'add_players_to_season', to: 'seasons#add_players_to_season'
   resources :players
 end
