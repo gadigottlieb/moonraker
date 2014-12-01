9e426ca373dad3225ecbe8ba097b0d2b25a3ba7b
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-30 23:42:24 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'visitors#team_info'
+  get '/all_users', to: 'visitors#all_users'
   devise_for :users
   resources :users
   resources :seasons do
-    resources :games
+    resources :games, except: [:index]
   end
   post 'add_players_to_season', to: 'seasons#add_players_to_season'
   resources :players
