369b9b18de2444c6071e62296fb95766e0338c13
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-11-30 23:36:24 -0500

    Updated stats methods

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -3,8 +3,19 @@ class Statistic < ActiveRecord::Base
   belongs_to :season
   belongs_to :game
 
-  def batting_avg(hits, at_bats)
-    string = (hits.to_f / at_bats.to_f).round(3)
+  def player_name(id)
+    "#{Player.find(id).last_name},  #{Player.find(id).first_name[0]} #{Player.find(id).primary_position}"
+  end
+
+  def at_bats(stat)
+    game_plate_appearance = stat.plate_appearance
+    game_walks = stat.walks
+    game_sac_fly = stat.sac_fly
+    @at_bats = game_plate_appearance - (game_walks + game_sac_fly)
+  end
+
+  def batting_avg(hits)
+    string = (hits.to_f / @at_bats.to_f).round(3)
     sprintf('%.3f', string)
   end
 
@@ -13,13 +24,15 @@ class Statistic < ActiveRecord::Base
     singles + (2 * doubles) + (3 * triples) + (4 * homers)
   end
 
+  def on_base_pct(hits, walks, at_bats, sac_fly)
+    string = (hits.to_f + walks.to_f) / (at_bats.to_f + walks.to_f + sac_fly.to_f)
+    sprintf('%.3f', string)
+  end
+
   def slugging_pct(total_bases, at_bats)
     string = (total_bases.to_f / at_bats.to_f).round(3)
     sprintf('%.3f', string)
   end
 
-  def on_base_pct(hits, walks, at_bats, sac_fly)
-    string = (hits.to_f + walks.to_f) / (at_bats.to_f + walks.to_f + sac_fly.to_f)
-    sprintf('%.3f', string)
-  end
+
 end
